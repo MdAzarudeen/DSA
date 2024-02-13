3ac7186a6db824b33e9018cdb420b1a59a4c11ee
MdAzarudeen <mohazar671@gmail.com>
2024-02-13 22:40:06 +0530

    commit 4

---
+++ Arrays/19.Longest Consecutive subsequence.java
@@ -0,0 +1,88 @@
+https://www.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(String args[])
+    {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+
+            for(int i=0; i<n; i++)
+                a[i] = sc.nextInt();
+            out.println(new Solution().findLongestConseqSubseq(a, n));
+            t--;
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //if we avoid sort function, we need to use Set
+    static int findLongestConseqSubseq(int arr[], int n)
+    {
+        Arrays.sort(arr);
+        int lastSmaller = Integer.MIN_VALUE;
+        int longest = 1;
+        int ct = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]-1==lastSmaller)
+            {
+                ct++;
+                lastSmaller = arr[i];
+            }
+            else if(arr[i]!=lastSmaller)
+            {
+                ct = 1;
+                lastSmaller = arr[i];
+            }
+            longest = Math.max(longest,ct);
+        }
+        return longest;
+    }
+}
---
+++ Arrays/22.Rotate Matrix anti-clockwise.java
@@ -0,0 +1,72 @@
+https://www.geeksforgeeks.org/problems/rotate-by-90-degree-1587115621/1
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
+
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int arr[][], int n)
+    {
+        int m = arr[0].length;
+        //Transpose
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i; j<m; j++)
+            {
+                int temp = arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        // Reverse
+        for(int j=0; j<m; j++)
+        {
+            for(int i=0; i<n/2; i++)
+            {
+                int temp = arr[i][j];
+                arr[i][j]=arr[n-1-i][j];
+                arr[n-1-i][j]=temp;
+            }
+        }
+
+    }
+}
